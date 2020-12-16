2355eca4c5f38c6514797f55221d7fca22c47ecb
Wichayut Ketsith <wichayut.ks@gmail.com>
2020-12-16 10:34:27 +0700

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Helloasdasd World</h1>'
+    return '<h1>Helloasdasd ;koWorld</h1>'
 
 
 if __name__ == '__main__':
