a5272fb944373c2c3c5146bd73670c1979d56a98
Wichayut Ketsith <wichayut.ks@gmail.com>
2020-12-16 10:41:19 +0700

    dasd

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Helloasdasd ;koWorld</h1>'
-
+    return '<h1>Helloasdasd ;dddddkoWorld</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
