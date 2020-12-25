b44b0845c87924fdfd5859a349330455b0f59f35
Thanaboon Sirirungnapawilai <thanaboon.sirirungnapawilai@gmail.com>
2020-12-25 11:04:49 +0700

    app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from json import encoder
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 import json
 import pyrebase
-from firebase_admin import auth
+import firebase_admin
+from firebase_admin import credentials, auth
 
 with open("config.json", encoding='utf 8') as json_file:
     data = json.load(json_file)
@@ -13,6 +13,9 @@ with open("config.json", encoding='utf 8') as json_file:
 
 # db.child('food').set({'key' : 'value'})
 
+cred = credentials.Certificate("loginjson.json")
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 
@@ -20,28 +23,37 @@ app = Flask(__name__)
 @app.route('/test')
 def test():
     return render_template('test.html')
-# @app.route('/index')
-# def index():
-#     return render_template('index.html')
 
 
-@app.route('/loginsignup', methods=["GET", "POST"])
-def login_signup():
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     if request.method == "GET":
-        return render_template("loginSignup.html")
+        return render_template("signup.html")
     elif request.method == "POST":
-        firstname = request.form["firstname"]
-        lastname = request.form["lastname"]
-        email = request.form["email"]
+        firstname = request.form['firstname'] 
+        lastname = request.form['lastname']
+        email = request.form['email']
         username = request.form['username']
         password = request.form['password']
-        user = auth.create_user(email=email, password=password, display_name=username)
-        data = {'firstname':firstname, 'lastname':lastname, 'email':user.email,
-                'password':password, 'userToken':user.uid, 'username':user.display_name}
-        db.child('food').push(data)
-        return render_template('login.html')
-        
 
+        try:
+            user = auth.create_user(email=email, password=password, display_name=username)
+            data = {'firstname':firstname, 'lastname':lastname, 'email':user.email, 
+                    'username':user.display_name, 'password':password, 'userToken':user.uid}
+            db.child('food').push(data)
+            return redirect(url_for("login"))
+        except:
+            return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        pb.auth().sign_in_with_email_and_password(email, password)
+        return redirect(url_for("test"))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
